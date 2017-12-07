47e644eaf2749d01c3a8a47f2417d644d008675b
Donald Tyler <domhnall.tilliere@gmail.com>
2017-12-06 19:52:39 -0600

    Change to PHP 7 for dev (not runtime)

--- docker/lib/images.sh
+++ docker/lib/images.sh
@@ -2,9 +2,5 @@
 
 # Environmental variables that define the various Docker images used in this project
 
-PHP_ACCOUNT=chekote
-PHP_REPO=noun-store-php
-PHP_TAG=2017-08-02
-
 SED_REPO=alpine
 SED_TAG=edge
