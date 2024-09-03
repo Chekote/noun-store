1b03c2d8bc5b2a0e99add3efeb2eda0d705e2641
Chekoté <mr@chekote.com>
2024-09-03 14:50:05 -0500

    Explicitly export vars used outside of the script that declares them

--- bin/lib/settings.sh
+++ bin/lib/settings.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
 COMPOSER_VERSION=2.7.8
+
+export COMPOSER_VERSION
--- docker/lib/tty.sh
+++ docker/lib/tty.sh
@@ -8,3 +8,5 @@ if [ -t 0 ]; then
   # Yes, set the TTY environmental variable to '-t' so when passed to Docker, the containers will also use it.
   TTY=-t
 fi
+
+export TTY
