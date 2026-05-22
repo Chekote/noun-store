ff4f98e963b777ba6140f6479b614c77a99068b6
Daniel Castro <danrcastro@gmail.com>
2026-05-22 13:28:51 -0300

    Add compatibility with PHP 8.4 (#66)
    * Fix PHP 8.4 deprecation
    
    PHP 8.4 has deprecated implicit null types. Previous versions would infer that the type is nullable because of the null default value. PHP 8.4 still allows it but throws a deprecation notice.
    
    https://php.watch/versions/8.4/implicitly-marking-parameter-type-nullable-deprecated
    
    * Bump illuminate/support to version 10
    
    The package is locked at version 5, released in 2015. This commit bumps the dependency to version 10, which is the latest version compatible with PHP 8.1 while also being compatible with 8.4.
    
    This fixes deprecation notices when running tests.
    
    * Bump phake/phake to version 4.7
    
    The package is locked at version 4.5. This commit bumps the dependency to version 4.7, which has fixed PHP 8.1 deprecations. This fixes the remaining deprecation notices when running tests.
    
    * Update Scrutinizer runner to use PHP 8.1.31
    
    * Fix Scrutinizer failing to run PHPCS
    
    Problem:
    
    Scrutinizer fails to run PHPCS with the following error message:
    
    PHP Fatal error: Array and string offset access syntax with curly braces is no longer supported in /home/scrutinizer/.analysis/phpcs/vendor/squizlabs/php_codesniffer/src/Config.php on line 399
    
    Cause:
    
    The Scrutinizer runtime comes with PHPCS pre-installed. The problem is that the pre-installed version is too old and incompatible with PHP 8.1.
    
    Solution:
    
    The solution is to add a compatible version to the project's dev dependencies and remove the use_website_config, allowing the project to control version and configuration for PHPCS directly.
    
    https://scrutinizer-ci.com/docs/tools/php/code-sniffer/#using-a-specific-php-code-sniffer-version
    
    * Revert illuminate/support upgrade

--- src/Chekote/NounStore/Store.php
+++ src/Chekote/NounStore/Store.php
@@ -13,7 +13,7 @@ class Store
      * @param Key|null $keyService the key service to use for parsing and building keys
      * @codeCoverageIgnore
      */
-    public function __construct(Key $keyService = null)
+    public function __construct(?Key $keyService = null)
     {
         $this->keyService = $keyService ?: Key::getInstance();
     }
